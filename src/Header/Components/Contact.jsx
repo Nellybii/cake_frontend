773de33ed80532df95d378298295fbf6d9fdfa62
IanGichane <gichaneian@gmail.com>
2024-01-24 13:47:41 +0300

    Contact icons

--- src/Header/Components/Contact.jsx
+++ src/Header/Components/Contact.jsx
@@ -1,4 +1,8 @@
 import { useState } from 'react';
+
+
+import { FaWhatsapp, FaInstagram, FaFacebook } from "react-icons/fa";
+import { IoCallOutline } from "react-icons/io5";
 function Contact() {
   const [inputs, setInputs] = useState({
     firstname: '',
@@ -21,66 +25,66 @@ function Contact() {
   return (
     <>
       <h1>CONTACT US</h1>
-      <div>
-        <p>icons</p>
-        <p>FAcebook</p>
-        <p>instagram</p>
-        <p>Whatapp</p>
+      <div className='icons'>
+        <a href="#"><FaFacebook /></a>
+        <a href="#"> <FaInstagram /></a>
+        <a href="#"><FaWhatsapp /></a>
+        <a href="#"> <IoCallOutline /></a>
       </div>
       <div className="contact-container">
-        <div>
+        <div className='contact-'>
           <div className="form-title">
-          <h1>Get in touch</h1>
+            <h1>Get in touch</h1>
           </div>
           <div>
             <form onSubmit={handleSubmit}>
 
-           <div className="input-wrapper">
-           <input
-                type="text"
-                name="firstname"
-                value={inputs.firstname || ""}
-                placeholder="First name "
-                onChange={handleChange}
-              />
+              <div className="input-wrapper">
+                <input
+                  type="text"
+                  name="firstname"
+                  value={inputs.firstname || ""}
+                  placeholder="First name "
+                  onChange={handleChange}
+                />
 
 
-              <input
-                type="text"
-                name="lastname"
-                value={inputs.lastname || ""}
-                placeholder="Last name"
-                onChange={handleChange}
-                required
-              />
-              <input
-                type="number"
-                name="phone"
-                value={inputs.phone || ""}
-                placeholder="Phone number"
-                onChange={handleChange}
-                required
-              />
-              <input
-                type="email"
-                name="email"
-                value={inputs.email || ""}
-                placeholder="Email "
-                onChange={handleChange}
-                required
-              />
-              <input
-                name="text"
-                value={inputs.text || ""}
-                placeholder="Text area "
-                onChange={handleChange}
-                required
-              />
+                <input
+                  type="text"
+                  name="lastname"
+                  value={inputs.lastname || ""}
+                  placeholder="Last name"
+                  onChange={handleChange}
+                  required
+                />
+                <input
+                  type="number"
+                  name="phone"
+                  value={inputs.phone || ""}
+                  placeholder="Phone number"
+                  onChange={handleChange}
+                  required
+                />
+                <input
+                  type="email"
+                  name="email"
+                  value={inputs.email || ""}
+                  placeholder="Email "
+                  onChange={handleChange}
+                  required
+                />
+                <input
+                  name="text"
+                  value={inputs.text || ""}
+                  placeholder="Text area "
+                  onChange={handleChange}
+                  required
+                />
 
-              <input 
-              
-              type="Submit" />
-           </div>
+                <input
+                  id="submit"
+                  type="Submit" />
+              </div>
             </form>
           </div>
         </div>
