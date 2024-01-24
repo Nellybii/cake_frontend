442c4706d2d963f84a7cd43f3824191e065fdcaa
IanGichane <gichaneian@gmail.com>
2024-01-24 14:12:34 +0300

    Footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Navigation from "./Header/Components/Navigation"
+import Footer from "./Footer/Footer";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <>
     <Navigation/>
+    <Footer/>
+
     </>
   )
 }
