3e85d26caf7a288958dd308affa9454402f945f6
David <dave.obat@student.moringashool.com>
2024-01-24 10:45:02 +0300

    homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Navigation from "./Header/Components/Navigation"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -8,26 +7,6 @@ function App() {
     <Navigation/>
     </>
   )
-=======
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import NavBarComp from "./Components/NavBarComp";
-import { Route, Routes } from "react-router-dom";
-import Home from "./Components/Home";
-import User from "./Components/User";
-
-function App() {
-  return (
-    <div>
-      <NavBarComp />
-      
-      <Routes>
-        <Route path="/home" element={<Home />}></Route>
-      </Routes>
-      <User />
-    </div>
-  );
->>>>>>> main
 }
 
 export default App
