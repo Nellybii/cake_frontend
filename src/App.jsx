7bf4457850c5b4710fde2ff8f8724d35bbdb97ae
nchore <nchoreanthony@gmail.com>
2024-01-28 00:07:48 +0300

    completed some task

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
-<<<<<<< HEAD
-=======
   
->>>>>>> ecca1e47a248570d7167c8ebb1e75f253fcd0e26
   return (
     <>
     <Navigation/>
